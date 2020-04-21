fdc97a2a16fcdec3353a63e71968a3e17d351539
keviveks <keviveks@gmail.com>
2020-04-21 15:54:50 +0530

    Next getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-const IndexPage = () => {
+import axios from 'axios';
+
+const IndexPage = ({ posts }) => {
     return (
         <div>
           <h1>Index Page</h1>
+          {posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
         </div>
     );
 }
+
+IndexPage.getInitialProps = async (appContext) => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const { data } = response;
+  return { posts: data };
+};
 
 export default IndexPage;
