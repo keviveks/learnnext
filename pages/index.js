8c3c0e6c66c22f2698c664e6922929284c18b02a
keviveks <keviveks@gmail.com>
2020-04-21 13:21:08 +0530

    Next app layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Link from 'next/link';
-
 const IndexPage = () => {
     return (
         <div>
-          <Link href="/about"><a>About Page</a></Link>
-          <Link href="/contact"><a>Contact Page</a></Link>
           <h1>Index Page</h1>
         </div>
     );
