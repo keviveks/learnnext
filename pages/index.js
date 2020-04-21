94584bd1686890ad6da27984c82819d90f58eeaa
keviveks <keviveks@gmail.com>
2020-04-21 17:16:43 +0530

    Serve with express for custom url

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
+import Link from 'next/link';
 
 const IndexPage = ({ posts }) => {
     return (
         <div>
           <h1>Index Page</h1>
           {posts.map(post => (
-            <li key={post.id}>{post.title}</li>
+            <li key={post.id}>
+              <Link href={`/post?id=${post.id}`} as={`/p/${post.id}`}><a>{post.title}</a></Link>
+            </li>
           ))}
         </div>
     );
